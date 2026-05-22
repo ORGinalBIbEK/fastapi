264fc1f07a8c9c76f543bf48be71d54a80873de5
Bibek <bibekmhrzn23@gmail.com>
2026-05-22 07:30:03 +0545

    some changes

--- database_practise/databse.py
+++ database_practise/databse.py
@@ -2,3 +2,21 @@ import sqlite3
 
 conn=sqlite3.connect('student.db')
 ##conn=sqlite3.connect(':memory:')
+
+c=conn.cursor()
+
+
+#create a table
+'''c.execute("""CREATE TABLE students(
+    id   INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age  INTEGER NOT NULL,
+    year INTEGER NOT NULL
+)""")'''
+#NUL,INTERGER,REAL(DECIMAL),TEXT,BLOBL(IMAGES,MP3)
+#commit our commit
+c.execute("INSERT INTO students VALUES (01, 'John', 20, 2021)")
+conn.commit()
+
+#close
+conn.close()

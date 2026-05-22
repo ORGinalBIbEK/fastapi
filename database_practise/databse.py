a9e076c7ac90c00d2bed17edd52f5cf3578a7cc7
Bibek <bibekmhrzn23@gmail.com>
2026-05-22 22:22:50 +0545

    some tessting

--- database_practise/databse.py
+++ database_practise/databse.py
@@ -5,17 +5,12 @@ conn=sqlite3.connect('student.db')
 
 c=conn.cursor()
 
+c.execute("SELECT * FROM students")
+#c.fetchone()
+#c.fetchmany()
+#c.fetchall()
+print(c.fetchall())
 
-#create a table
-'''c.execute("""CREATE TABLE students(
-    id   INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    age  INTEGER NOT NULL,
-    year INTEGER NOT NULL
-)""")'''
-#NUL,INTERGER,REAL(DECIMAL),TEXT,BLOBL(IMAGES,MP3)
-#commit our commit
-c.execute("INSERT INTO students VALUES (01, 'John', 20, 2021)")
 conn.commit()
 
 #close
